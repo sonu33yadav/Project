a884ab574ea71d0240f8fcd1b54d813a2c93ef44
sonu33yadav <sk1533884@gmail.com>
2021-06-17 12:51:09 +0530

    project created in laravel, model, seeder and controller generated Signed-off-by: sonu33yadav <sk1533884@gmail.com>

---
+++ app/Http/Controllers/EmailController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\email;
+use Illuminate\Http\Request;
+
+class EmailController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\email  $email
+     * @return \Illuminate\Http\Response
+     */
+    public function show(email $email)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\email  $email
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(email $email)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\email  $email
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, email $email)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\email  $email
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(email $email)
+    {
+        //
+    }
+}
